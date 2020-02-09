0c48fad0b70530448a4e4acca91178b000e03898
CoderiGenius <755717181@qq.com>
2020-02-09 19:17:17 +0800

    Create code.js

---
+++ code.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         微吼直播签到助手
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://live.vhall.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+function signIn() {
+    var buttons = document.getElementsByTagName("button");
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].innerHTML === "签到") {
+            buttons[i].click();
+            console.log("签到成功！");
+        }
+    }
+}
+setInterval(signIn, 1000);
+})();
